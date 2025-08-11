4d3d4015919745b5a0788a38f07b128c9f70c11a
Earthenmist <jamierhirst@icloud.com>
2025-08-11 19:07:16 +0100

    Create enUS.lua

---
+++ Locales/enUS.lua
@@ -0,0 +1,19 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AltTracker", "enUS", true)
+if not L then return end
+
+-- General
+L["AltTracker"] = "AltTracker"
+L["Minimap Tooltip"] = "Left-click: Toggle | Right-click: Options"
+L["Enabled"] = "Enabled"
+L["Minimap Button"] = "Show Minimap Button"
+L["General"] = "General"
+
+-- Modules
+L["Character Overview"] = "Character Overview"
+L["Currency Tracker"] = "Currency Tracker"
+L["Reputation Monitor"] = "Reputation Monitor"
+L["Quest Progress"] = "Quest Progress"
+L["Inventory & Bank Sync"] = "Inventory & Bank Sync"
+L["Profession Status"] = "Profession Status"
+L["Raid Lockouts & Mythic+"] = "Raid Lockouts & Mythic+"
+L["Achievements & Collections"] = "Achievements & Collections"
